89d7ccf2b37c623663042e1323ad05ec72e84dbc
Hary Prihadi <haryprihadi@gmail.com>
2020-06-29 21:48:44 +0700

    trying out responsive framework package

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_framework/responsive_framework.dart';
+import 'package:webprofile/shared/shared.dart';
 import 'package:webprofile/ui/pages/pages.dart';
 
 void main() {
@@ -12,6 +14,20 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      builder: (context, child) => ResponsiveWrapper.builder(
+          BouncingScrollWrapper.builder(context, child),
+          maxWidth: 1200,
+          minWidth: 450,
+          defaultScale: true,
+          breakpoints: [
+            ResponsiveBreakpoint.resize(450, name: MOBILE),
+            ResponsiveBreakpoint.autoScale(800, name: TABLET),
+            ResponsiveBreakpoint.resize(1200, name: DESKTOP),
+            ResponsiveBreakpoint.resize(2460, name: '4K'),
+          ],
+          background: Container(
+            color: backgroundColor,
+          )),
       home: MainPage(),
     );
   }
